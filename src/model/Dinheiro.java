1484eb1b296713e0aeba928896b48d8100d3a670
Darleneleao <darlene_leao@outlook.com>
2022-09-22 14:58:09 -0300

    criacao da classe Dinheiro

--- src/model/Dinheiro.java
+++ src/model/Dinheiro.java
@@ -1,5 +1,33 @@
 package model;
 
-public class Dinheiro {
+import java.sql.Time;
+
+public class Dinheiro extends Pagamento{
+	private double desconto;
+	
+	public Dinheiro(Time hora, double valor, double desconto) {
+		super(hora, valor);
+		this.desconto = desconto/100;
+	}
+	
+	public double getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(double desconto) {
+		this.desconto = desconto;
+	}
+
+	@Override
+	public String descricaoPagamento() {
+		return "Dinheiro";
+	
+	}
+	
+	@Override 
+	public double calcularValor() {
+		return getValor() - (getValor() * desconto);
+		
+	}
 
 }
